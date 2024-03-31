19ea2dada37584c0365b86d00a77ae156dc1135a
LECHIY94 <122964071+LECHIY94@users.noreply.github.com>
2024-03-31 03:43:16 +0300

    Update test_ls_1.py

--- test_ls_1.py
+++ test_ls_1.py
@@ -6,12 +6,12 @@ from task_1 import check_command
 import pytest
 
 
-# # Тест команды вывода списка файлов
-# command = "ls /home/user/folder_ex"
-# text = "file_1"
+
+ command = "ls /home/user/folder_ex"
+ text = "file_1"
 
 
-# Тест команды разархивирования с путями
+
 command = "tar -xf /home/user/folder_ex/archive.tar -C /home/user/folder_ex"
 text = "file_1"
 
